db66dd43e8b2f2d7af9cddcac29297c1b53b27a9
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-30 17:11:19 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+}, 0)
+
 // Code your solution here
+
+
